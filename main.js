42504f0d1d20eb7b851c8339ee900a21574d43f3
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2022-06-16 23:32:11 -0300

    Adicionado o arquivo main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  const body = document.querySelector('body')
+  body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  const body = document.querySelector('body')
+  body.classList.remove('menu-expanded')
+}
